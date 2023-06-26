7c1f16631587c679dc5bee6c429f6d23c26756ea
EvelineGomes <gomes.evepg@gmail.com>
2023-06-26 18:21:47 -0400

    added the App component and removed some comments

---
+++ src/App.js
@@ -0,0 +1,13 @@
+// This will be the starting point and the most important component of the entire application.
+
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello, World!</h1>
+    </div>
+  );
+};
+
+export default App;
