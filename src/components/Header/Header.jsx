adc4f399b936ce7017c8667b49f59f6acdcb66a5
EvelineGomes <gomes.evepg@gmail.com>
2023-06-28 17:06:07 -0400

    correcting some typos and commenting out Autocomplete because we still don't have everything needed

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,34 +1,36 @@
 import React from "react";
 import { Autocomplete } from "@react-google-maps/api";
 import { AppBar, Toolbar, Typography, InputBase, Box } from "@material-ui/core";
-// to use the SearchIcon in the Autocomplete component, we need to import it:
 import SearchIcon from "@material-ui/icons/Search";
 
+// now that we have the styles, we can import them:
+import useStyles from "./styles";
+
 const Header = () => {
+  //useStyles is a hook, so we need to call it as it is:
+  const classes = useStyles();
+
   return (
     <AppBar position="static">
       <Toolbar className={classes.toolbar}>
-        {/* Typography is every single text element and by using variant we can set it as headings, titles, subtitles etc */}
         <Typography variant="h5" className={classes.title}>
           Travel Advisor
         </Typography>
-        {/* Box is like a div */}
         <Box display="flex">
           <Typography variant="h6" className={classes.title}>
             Explore new places
           </Typography>
-          <Autocomplete>
-            <div className={classes.search}>
-              <div className={classes.searchIcon}>
-                <SearchIcon />
-              </div>
-              <InputBase
-                placeholder="Search..."
-                classes={{ root: classes.inputRoot, input: classes.inputInput }}
-              />
-              {/* To set the classes we used {} because we're setting more than one class, so it becomes an object and it needs to be inside {} */}
+          {/* <Autocomplete> */}
+          <div className={classes.search}>
+            <div className={classes.searchIcon}>
+              <SearchIcon />
             </div>
-          </Autocomplete>
+            <InputBase
+              placeholder="Search..."
+              classes={{ root: classes.inputRoot, input: classes.inputInput }}
+            />
+          </div>
+          {/* </Autocomplete> */}
         </Box>
       </Toolbar>
     </AppBar>
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -1,4 +1,4 @@
-import { makeStyles } from "@material-ui/core/styles";
+import { alpha, makeStyles } from "@material-ui/core/styles";
 
 export default makeStyles((theme) => ({
   title: {
@@ -11,7 +11,7 @@ export default makeStyles((theme) => ({
     position: "relative",
     borderRadius: theme.shape.borderRadius,
     backgroundColor: alpha(theme.palette.common.white, 0.15),
-    "&:hover": { backgroundColor: alpha(theme.pallete.common.white, 0.25) },
+    "&:hover": { backgroundColor: alpha(theme.palette.common.white, 0.25) },
     marginRight: theme.spacing(2),
     marginLeft: 0,
     width: "100%",
@@ -19,28 +19,28 @@ export default makeStyles((theme) => ({
       marginLeft: theme.spacing(3),
       width: "auto",
     },
-    searchIcon: {
-      padding: theme.spacing(0, 2),
-      height: "100%",
-      position: "absolute",
-      pointerEvents: "none",
-      display: "flex",
-      alignItems: "center",
-      justifyContent: "center",
-    },
-    inputRoot: {
-      color: "inherit",
-    },
-    inputInput: {
-      padding: theme.spacing(1, 1, 1, 0),
-      paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
-      transition: theme.transitions.create("width"),
-      width: "100%",
-      [theme.breakpoints.up("md")]: { width: "20ch" },
-    },
-    toolbar: {
-      display: "flex",
-      justifyContent: "space-between",
-    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: "100%",
+    position: "absolute",
+    pointerEvents: "none",
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  inputRoot: {
+    color: "inherit",
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create("width"),
+    width: "100%",
+    [theme.breakpoints.up("md")]: { width: "20ch" },
+  },
+  toolbar: {
+    display: "flex",
+    justifyContent: "space-between",
   },
 }));
