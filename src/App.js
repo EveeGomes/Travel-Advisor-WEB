54371e9ddf924c6f6944c42b5bbf1d6f9247fc84
EvelineGomes <gomes.evepg@gmail.com>
2023-06-28 16:11:41 -0400

    adding components to App as well as using material-ui components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,23 @@
-// This will be the starting point and the most important component of the entire application.
-
 import React from "react";
+// we can also import from material-ui
+import { CssBaseline, Grid } from "@material-ui/core";
+
+// Now we can import the components here
+import Header from "./components/Header/Header";
+import List from "./components/List/List";
+import Map from "./components/Map/Map";
 
 const App = () => {
   return (
-    <div>
-      <h1>Hello, World!</h1>
-    </div>
+    <>
+      <CssBaseline />
+      <Header />
+      {/* since we're doing an inline style, it'll be an object! So that's why it needs to be inside curly braces!!!!! */}
+      <Grid container spacing={3} style={{ width: "100%" }}>
+        <Grid item xs={12} md={4}></Grid>
+      </Grid>
+      {/* First Grid is of type container while the second is of type item */}
+    </>
   );
 };
 
